bdd56492122693d4cdcfa4d388bddb971517ebb8
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-03-24 12:23:27 -0400

    mobile background css fits enitire page

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ const About = () => {
 				I am a Full-stack developer with a Bachelor’s Degree in Criminal Justice and possess a strong interest 
 				in React and Javascript. As a former NYPD police officer for five years, I gained skills in problem-solving, 
 				being resourceful in high stress situations, and empathetically resolving the situations at hand. Along with 
-				this I collaborate with different units and precincts. 
+				this I collaborated with different units and precincts. 
 				</p>
 				<p className='aboutPrgh'>
 				I use those same strengths when programming. Whether it 
@@ -25,16 +25,3 @@ const About = () => {
 };
 
 export default About;
-
-			//<img className='imgAbout' src={oliveTree} alt='Olive Tree branches' />
-
-// I am a Full-stack developer with a Bachelor’s Degree in Criminal Justice and possess a strong interest 
-// in React and Javascript. As a former NYPD police officer for five years, I gained skills in problem-solving, 
-// being resourceful in high stress situations, and empathetically resolving the situations at hand. Along with 
-// this I collaborate with different units and precincts. 
-
-// I use those same strengths when programming. Whether it 
-// is a bug in my application, collaborating with other team members to get a project completed, or when helping a 
-// fellow peer debug. I have a unique ability to approach a problem from an outsider perspective. My past experiences 
-// will allow me to foster an environment that prioritizes patience, empathy, and collaboration in order to be a great 
-// teammate and programmer. 
