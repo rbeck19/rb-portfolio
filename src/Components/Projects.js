5162abc898c0a82a2cd9c7d37a410464e4a2efe2
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-11-20 17:11:10 -0500

    added projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,12 +5,55 @@ import recipeFoodForumMain from '../img/recipe-forum.png';
 import passportMain from '../img/passport-main.png';
 import bookshelfMain from "../img/bookshelf-main.png";
 import slotMachine from "../img/slot-machine.png";
-
-
+import petParent from "../img/pet-parent.png"
+import platePilot from "../img/plate-pilot.png"
 
 const Projects = () => {
 	return (
 		<div>
+			<div className='projectsContainer'>
+				<div className='projectsCard'>
+					<h1 className='projectsTitle'>Plate Pilot</h1>
+					<p className='projectStack'> React | MongoDb </p>
+					<a className='buttonLink' href='https://github.com/lucas-richards/23oct-hackathon-FrontEnd' target='_blank' rel="noopener noreferrer">
+						View Code
+					</a>
+					<a className='buttonLink' href='https://plate-pilot.onrender.com/ ' target='_blank' rel="noopener noreferrer">
+						View Live
+					</a>
+					<p className='projectDesc'>
+						Collaborated with UX designers and developers to create a MERN Stack website.
+					</p>
+					<p className='projectDesc'>
+					The application integrates the Yelp API to enhance the dining decision-making process. Leveraging the power of the Yelp API, the application offers users the ability to make randomized restaurant selections tailored to their preferences through a set of user-defined filters.					</p>					
+					<div className='keyImages'>
+						<img className='projectImages' src={platePilot} alt='Main page of pet parent' />
+					
+					</div>
+				</div>
+			</div>			
+			<div className='projectsContainer'>
+				<div className='projectsCard'>
+					<h1 className='projectsTitle'>Pet Parent</h1>
+					<p className='projectStack'> React | MongoDb </p>
+					<a className='buttonLink' href=' https://github.com/ChelseyReann/nam1_hackathon_frontend' target='_blank' rel="noopener noreferrer">
+						View Code
+					</a>
+					<a className='buttonLink' href='https://musical-mousse-373f3d.netlify.app/' target='_blank' rel="noopener noreferrer">
+						View Live
+					</a>
+					<p className='projectDesc'>
+						Collaborated with UX designers and developers to create a MERN Stack website.
+					</p>
+					<p className='projectDesc'>
+						The platform serves as a pet community, providing users with the opportunity to share their own pet-related content and discover posts from fellow pet enthusiasts. Whether it's  sharing valuable pet care tips, or exchanging heartwarming stories, the website fosters a lively space for all things pet-related. 
+					</p>					
+					<div className='keyImages'>
+						<img className='projectImages' src={petParent} alt='Main page of pet parent' />
+					
+					</div>
+				</div>
+			</div>
 			<div className='projectsContainer'>
 				<div className='projectsCard'>
 					<h1 className='projectsTitle'>Recipe Food Forum</h1>
@@ -26,7 +69,7 @@ const Projects = () => {
 						favorite recipes and comment on recipes.
 					</p>
 					<p className='projectDesc'>
-						Collaborated with a team of 5 and functioned as a Fullstack Developer using React, Python, Django,
+						Collaborated with a team of developers and functioned as a Fullstack Developer using React, Python, Django,
 						and implementing all CRUD endpoints (create, read, update, delete) that were received from the server side.
 						As well as implementing client-side authentication and authorization routes.
 					</p>
@@ -51,7 +94,7 @@ const Projects = () => {
 						and the memories associated with that trip.
 					</p>
 					<p className='projectDesc'>
-						Collaborated with a team of 5 and functioned as a Fullstack Developer using React, MongoDb,
+						Collaborated with a team of developers and functioned as a Fullstack Developer using React, MongoDb,
 						and implementing all CRUD endpoints (create, read, update, delete) that were received from the server side.
 						As well as implementing client-side authentication and authorization routes.
 					</p>
